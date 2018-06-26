45d6b2cf1d2a8217e90cd90cd599cfea065a626a
whoopsie <m6c7l@web.de>
2018-06-26 17:58:56 +0200

    Update xps_utils.R

--- dashboard/xps_utils.R
+++ dashboard/xps_utils.R
@@ -1,5 +1,5 @@
 require(httr)
-xpsEndpoint <- "http://cosa-app.fh-luebeck.de:50101/"
+xpsEndpoint <- "http://[host]:[port]/"
 sendModelToXPS <- function(model) {
   
   POST(xpsEndpoint, add_headers(Authorization = "Token b5b41fac0361d157d9673ecb926af5ae"), body = toJSON(model))
