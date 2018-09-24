151396218464c6bebd1bbdeaba553820c225254c
hecking <hecking@collide.info>
2018-09-24 16:03:22 +0200

    localhost

--- dashboard/config.R
+++ dashboard/config.R
@@ -1,5 +1,5 @@
 appConfig <- list(
   xpsEndpoint = "http://cosa-app.fh-luebeck.de:50101",
-  managementEndpoint = "http://descartes.inf.uni-due.de:5000/management",
-  dataEndpoint = "http://descartes.inf.uni-due.de:5000"
+  managementEndpoint = "http://localhost:5000/management",
+  dataEndpoint = "http://localhost:5000"
 )
