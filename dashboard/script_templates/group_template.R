76aeaf4979fce516456ab753e53c3d45a7a5c205
doberstein <doberstein@unitybox.de>
2018-09-25 16:55:58 +0200

    changed model/script to fit current specification

--- dashboard/script_templates/group_template.R
+++ dashboard/script_templates/group_template.R
@@ -2,19 +2,47 @@ source("config.R")
 source("xps_utils.R")
 source("data_utils.R")
 
-groupSequences <- getGroupSequencesAll(course)
+# inputs:
+# paste("course <- '", model$model_metadata$course_id, "'", sep=""),
+# paste("pFrom <- '", model$model_metadata$period_from, "'", sep=""),
+# paste("pTo <- '", model$model_metadata$period_to, "'", sep=""),
+# paste("taskId <- '", model$model_metadata$task_context$task_id, "'", sep=""))
 
-groupLatencies <- getGroupLatencies2(groupSequences, pFrom, pTo)
 
-sequences=(groupSequences %>% group_by(group_id) %>% do(sequence=select(., -group_id)))
+# task sequences for course and task
+groupTaskSequences <- getGroupTaskSequencesAll(course, taskId)
 
-list(
-  model_metadata=list(
-    course_id=course, 
-    period_from=pFrom,
-    period_to=pTo
-  ),
-  latencies=groupLatencies,
-  # add commit latencies to list
-  sequences=(groupSequences %>% group_by(group_id) %>% do(sequence=select(., -group_id)))
-) %>% sendModelToXPS
+# latencies from course, do not include forum inactive groups
+groupLatencies <- getGroupLatencies2(groupTaskSequences, pFrom, pTo)
+
+# selected task
+task <- getTaskListForCourse(course) %>% filter(task_id == taskId)
+
+# all groups and users for task
+groupsAndUsers <- getGroupsAndUsersForCourse(course, taskId)
+
+# include forum inactive groups in the latencies with latency = 0
+latencies <-getAllLatenciesFun(groupLatencies, groupsAndUsers)
+
+work_imbalance <- calculateWorkImbalanceFun(groupTaskSequences, groupsAndUsers)
+
+forum_sequences <- groupTaskSequences %>% filter(verb_id == "http://id.tincanapi.com/verb/replied")
+wiki_sequences <- groupTaskSequences %>% filter(verb_id == "http://id.tincanapi.com/verb/updated")
+
+group_sequences <- groupTaskSequences %>% filter(verb_id == "http://id.tincanapi.com/verb/replied" | verb_id == "http://id.tincanapi.com/verb/updated") %>%  group_by(group_id) %>% do(sequence=select(., -c(group_id, content)))
+
+forum_wordcount <- calculateForumWordcountFun(forum_sequences, groupsAndUsers)
+wiki_wordcount <- calculateWikiWordcountFun(wiki_sequences, groupsAndUsers)
+
+model <- buildGroupModel(course = course,
+                from = pFrom,
+                to = pTo,
+                task = task,
+                groups = groupsAndUsers,
+                average_latencies = groupLatencies,
+                work_imbalance = work_imbalance,
+                text_contribution_forum = forum_wordcount,
+                text_contribution_wiki = wiki_wordcount,
+                group_sequences = group_sequences)
+
+model %>% sendModelToXPS
