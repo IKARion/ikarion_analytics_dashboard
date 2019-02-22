93fda35170df89eb95eefc957d468a9a2282a21b
doberstein <dorian.doberstein@unitybox.de>
2019-02-22 17:02:58 +0100

    dummy data

---
+++ dashboard/dummy_data.R
@@ -0,0 +1,5 @@
+dummy_SA_T1 <- data.frame("group_id" = c(305,305,304,304), "timestamp" = c(1548025051, 1548024981, 1548024766, 1548024629), "user_id" = c("wdqshw4nFEYjiBA+UokJ5w==", "nlrrOSj7CO4Pk21IIcDnog==", "tLbffKo5+/MJtqFFCnIWEg==", "EA12iYsCduiMrz+xSdFQjQ=="), "item1" = c(0,1,0,4), "item2" = c(2,1,0,4), "item3" = c(4,1,0,3))
+
+dummy_WFW_T1 <- data.frame("group_id" = c(305,305,304,304), "timestamp" = c(1548025051, 1548024981, 1548024766, 1548024629), "user_id" = c("wdqshw4nFEYjiBA+UokJ5w==", "nlrrOSj7CO4Pk21IIcDnog==", "tLbffKo5+/MJtqFFCnIWEg==", "EA12iYsCduiMrz+xSdFQjQ=="), "weighted_forum_wordcount" = c(0,0.3,0.255,1))
+
+dummy_WWW_T1 <- data.frame("group_id" = c(305,305,304,304), "timestamp" = c(1548025051, 1548024981, 1548024766, 1548024629), "user_id" = c("wdqshw4nFEYjiBA+UokJ5w==", "nlrrOSj7CO4Pk21IIcDnog==", "tLbffKo5+/MJtqFFCnIWEg==", "EA12iYsCduiMrz+xSdFQjQ=="), "weighted_wiki_wordcount" = c(1,0.2,0.45,0.5))
