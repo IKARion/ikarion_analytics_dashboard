d815f16b0dbab19724f867c67d779f12fe480439
doberstein <dorian.doberstein@unitybox.de>
2019-02-22 17:08:31 +0100

    + self assessments + weighted forum wordcount + weighted wiki wordcount
    - work imbalance
    - forum wordcount
    - wiki wordcount

--- dashboard/script_templates/group_template.R
+++ dashboard/script_templates/group_template.R
@@ -13,7 +13,7 @@ source("data_utils.R")
 groupTaskSequences <- getGroupTaskSequencesAll(course, taskId)
 
 # latencies from course, do not include forum inactive groups
-groupLatencies <- getGroupLatencies2(groupTaskSequences, pFrom, pTo)
+# groupLatencies <- getGroupLatencies2(groupTaskSequences, pFrom, pTo)
 
 # selected task
 task <- getTaskListForCourse(course) %>% filter(task_id == taskId)
@@ -22,28 +22,64 @@ task <- getTaskListForCourse(course) %>% filter(task_id == taskId)
 groupsAndUsers <- getGroupsAndUsersForCourse(course, taskId)
 
 # include forum inactive groups in the latencies with latency = 0
-latencies <-getAllLatenciesFun(groupLatencies, groupsAndUsers)
+# latencies <-getAllLatenciesFun(groupLatencies, groupsAndUsers)
 
-work_imbalance <- calculateWorkImbalanceFun(groupTaskSequences, groupsAndUsers)
+#work_imbalance <- calculateWorkImbalanceFun(groupTaskSequences, groupsAndUsers)
 
-forum_sequences <- groupTaskSequences %>% filter(verb_id == "http://id.tincanapi.com/verb/replied")
-wiki_sequences <- groupTaskSequences %>% filter(verb_id == "http://id.tincanapi.com/verb/updated")
+#forum_sequences <- groupTaskSequences %>% filter(verb_id == "http://id.tincanapi.com/verb/replied")
+#wiki_sequences <- groupTaskSequences %>% filter(verb_id == "http://id.tincanapi.com/verb/updated")
 
 #group_sequences <- groupTaskSequences %>% filter(verb_id == "http://id.tincanapi.com/verb/replied" | verb_id == "http://id.tincanapi.com/verb/updated") %>%  group_by(group_id) %>% do(sequence=select(., -c(group_id, content)))
-group_sequences <- generateGroupTaskSequences( groupTaskSequences, groupsAndUsers)
+group_sequences <- generateGroupTaskSequences( groupTaskSequences, groupsAndUsers, task)
 
 forum_wordcount <- calculateForumWordcountFun(forum_sequences, groupsAndUsers)
 wiki_wordcount <- calculateWikiWordcountFun(wiki_sequences, groupsAndUsers)
 
-model <- buildGroupModel(course = course,
-                from = pFrom,
-                to = pTo,
-                task = task,
-                groups = groupsAndUsers,
-                #average_latencies = groupLatencies,
-                work_imbalance = work_imbalance,
-                text_contribution_forum = forum_wordcount,
-                text_contribution_wiki = wiki_wordcount,
-                group_sequences = group_sequences)
+### !!! ###
+# ADD SELFASSESSMENT HERE AS WELL
+
+weighted_forum_wordcount <- getGroupWeightedForumWordcountAll(course, taskId, as.numeric(Sys.time()))
+weighted_wiki_wordcount <- getGroupWeightedWikiWordcountAll(course, taskId, as.numeric(Sys.time()))
+
+
+self_assessment <- getGroupSelfAssessmentsAll(course, taskId, as.numeric(Sys.time()))
+
+# add check if a relevant activitiy occured (dim(gTS))
+# only then, calculate the values, otherwise call buildEmptyGroupModel with only task and group info
+
+model <- NULL
+
+if (dim(groupTaskSequences[1] == 0)) {
+  model <- buildEmptyGroupModel(course,
+                                task,
+                                groupsAndUsers)
+} else {
+  model <- buildGroupModel(course = course,
+                           #from = pFrom,
+                           #to = pTo,
+                           task = task,
+                           groups = groupsAndUsers,
+                           #average_latencies = groupLatencies,
+                           #work_imbalance = work_imbalance,
+                           #text_contribution_forum = forum_wordcount,
+                           #text_contribution_wiki = wiki_wordcount,
+                           
+                           weighted_forum_wordcount = weighted_forum_wordcount,
+                           weighted_wiki_wordcount = weighted_wiki_wordcount,
+                           self_assessment = self_assessment,
+                           
+                           group_sequences = group_sequences)
+}
+
+# model <- buildGroupModel(course = course,
+#                 from = pFrom,
+#                 to = pTo,
+#                 task = task,
+#                 groups = groupsAndUsers,
+#                 #average_latencies = groupLatencies,
+#                 work_imbalance = work_imbalance,
+#                 text_contribution_forum = forum_wordcount,
+#                 text_contribution_wiki = wiki_wordcount,
+#                 group_sequences = group_sequences)
 
 model %>% sendModelToXPS
